a4bf32dfb067bb6a4b6f67566117bc32b87d19d8
Antonios Vamvakos <Vamvakos@bausoftware.com>
2022-12-01 10:08:53 +0100

    ava-day1 and init

---
+++ 2022/Antonios/AdventOfCode/ConsoleApp1/dailytemplate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class DailyTemplate
+{   
+    public static void Resolve()
+    {     
+        Console.WriteLine("--------< Day X >-----------");
+        Console.WriteLine($"Result");
+        Console.WriteLine("---------------------------");
+    }
+}
